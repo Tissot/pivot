58022062f073ee5bf15affbf1482870e9d11d3ab
T-ming <759453441@qq.com>
2018-08-08 16:48:50 +0800

    update(throttle): 更新 throttle 。

--- throttle.js
+++ throttle.js
@@ -1,18 +1,27 @@
 'use strict';
 
 function throttle(func, wait = 0) {
+  let timer;
   let last;
   return (...args) => {
-    const now = new Date;
-    if (!last || now - last >= wait) {
+    if (!last) {
       func(...args);
-      last = now;
+      last = Date.now();
+    } else {
+      timer && clearTimeout(timer);
+      timer = setTimeout(() => {
+        if (Date.now() - last >= wait) {
+          func(...args);
+          last = Date.now();
+        }
+      }, wait - (Date.now() - last));
     }
-  };
+  }
 }
 
 let count = 1;
 const test = throttle(console.log, 1000);
+test(count++);
 test(count++);
 test(count++);
 setTimeout(() => {
@@ -20,6 +29,7 @@ setTimeout(() => {
   setTimeout(() => {
     test(count++);
     setTimeout(() => test(count++), 1200);
-  }, 800);
-}, 1200);
-// expected: 1 3 5
+  }, 700);
+}, 1300);
+
+// expected: 1 3 5 6
